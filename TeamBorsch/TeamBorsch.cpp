9534b9e66e35f219005c3334896787f4e31f2795
Oleksandr <skajrem@gmail.com>
2024-11-01 20:09:56 +0200

    add main

--- TeamBorsch/TeamBorsch.cpp
+++ TeamBorsch/TeamBorsch.cpp
@@ -5,5 +5,17 @@ using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int choice;
+    int a, b, c;
+    cout << "Hello" << endl << "Choose your operation(1.add, 2.subtract, 3.multiply)" << endl;
+    cin >> choice;
+    if (choice == 1) {
+        //add(a,b,c);
+    }
+    if (choice == 2) {
+        //subtract(a,b,c);
+    }
+    if (choice == 3) {
+        //multiply(a,b,c);
+    }
 }
